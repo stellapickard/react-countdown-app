609a8c9162c21aa51e5632028863a7f75a97a820
stellapickard <stella@pickard.co.za>
2018-03-25 22:22:11 +0200

    Countdown app complete

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Clock from './Clock.jsx'
+import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      deadline: 'December 25, 2018',
+      newDeadLine: ''
+    }
+  }
+
+  changeDeadline(){
+    this.setState({deadline: this.state.newDeadline});
+  }
+
+  render(){
+    return (
+    <div className='App'> 
+      <div className='title'>Countdown to {this.state.deadline} </div>
+      <Clock
+        deadline={this.state.deadline}
+      />
+      <Form inline>
+        <FormControl 
+          className='Deadline-input'
+          placeholder='deadline date'
+          onChange={event=> this.setState({newDeadline: event.target.value})}
+        />
+        <Button onClick={() => this.changeDeadline()}> 
+          Submit 
+        </Button>
+      </Form>
+    </div>
+    )
+  }
+}
+
+export default App;
